2dad3eed56a5b35c76c53db33ea3728ac46921fe
rinsuki <428rinsuki+git@gmail.com>
2022-08-07 21:39:39 +0900

    disconnect queue

--- addon/background.js
+++ addon/background.js
@@ -1,38 +1,66 @@
 const tabIdToConnection = new Map()
+/** @type {{conn: any, timestamp: Date}[]} */
+let disconnectQueue = []
+
+setInterval(() => {
+    if (disconnectQueue.length) {
+        disconnectQueue = disconnectQueue.filter(queue => {
+            if (queue.timestamp.getTime() < (Date.now() - 5000)) {
+                queue.conn.disconnect()
+                console.info("[nicorich] native connection was disconnected by queue", queue, Date.now())
+                return false
+            }
+            return true
+        })
+    }
+}, 1000)
 
 function gone(tabId) {
     let conn = tabIdToConnection.get(tabId)
     if (conn != null) {
-        conn.disconnect()
-        console.info("disconnected by gone", tabId)
+        disconnectQueue.push({
+            conn,
+            timestamp: new Date(),
+        })
     }
     tabIdToConnection.delete(tabId)
 }
 
 browser.runtime.onMessage.addListener((message, sender) => {
-    console.log(message)
-    if (sender.tab == null) return
-    const tabId = sender.tab.id
-    console.log(message)
-    if (message.type === "gone") {
-        gone(tabId)
-        return true
-    } else if (message.type === "playing") {
-        let conn = tabIdToConnection.get(tabId)
-        if (conn == null) {
-            conn = browser.runtime.connectNative("nicorich_native")
-            conn.onDisconnect.addListener(e => {
-                console.warn("disconnected", e)
-                if (tabIdToConnection.get(tabId) === conn) {
-                    tabIdToConnection.delete(tabId)
+    try {
+        console.log(message)
+        if (sender.tab == null) return
+        const tabId = sender.tab.id
+        console.log(message)
+        if (message.type === "gone") {
+            gone(tabId)
+            return true
+        } else if (message.type === "playing") {
+            let conn = tabIdToConnection.get(tabId)
+            if (conn == null) {
+                const connFromQueue = disconnectQueue.shift()
+                conn = connFromQueue?.conn
+                if (conn == null) {
+                    conn = browser.runtime.connectNative("nicorich_native")
+                    console.info("[nicorich] craete native connection")
+                } else {
+                    console.info("[nicorich] reuse connection from disconnect queue")
                 }
-            })
-            tabIdToConnection.set(tabId, conn)
+                conn.onDisconnect.addListener(e => {
+                    console.warn("disconnected", e)
+                    if (tabIdToConnection.get(tabId) === conn) {
+                        tabIdToConnection.delete(tabId)
+                    }
+                })
+                tabIdToConnection.set(tabId, conn)
+            }
+            conn.postMessage(message)
+            return true
+        } else {
+            console.warn("[nicorich] unknown message", message)
         }
-        conn.postMessage(message)
-        return true
-    } else {
-        console.warn("unknown message", message)
+    } catch(e) {
+        console.error("[nicorich] error in message handler", e)
     }
 })
 
